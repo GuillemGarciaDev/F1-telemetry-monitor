ea220dc37327d066913f079097ac2af6a3113511
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2022-06-23 13:46:09 +0200

    WIP: First version of sockets connection pending to test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,25 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { useEffect, useState } from 'react'
+import io from 'socket.io-client'
 import './App.css'
 
+const socket = io.connect('http://localhost:5050')
+
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [carStatus, setCarStatus] = useState(null)
+
+  useEffect(() => {
+    socket.on('carStatus', (data) => {
+      setCarStatus(data)
+    })
+  }, [socket])
+  
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <p>
+        {carStatus}
+      </p>
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,20 +1,38 @@
 const express = require('express')
 const F1TelemetryClient = require('f1-2021-udp')
+const { Server } = require('socket.io')
 
 /* Init server configuration */
 const app = express()
-const port = 5050
+const PORT = 5050
+const DEBUG = true
+const io = new Server()
+
+io.on('connection', (socket) => {
+    console.log('[ ✅ ] Client connected')
+    io.on('disconnect', () => {
+        console.log('[ ❌ ] Client disconnected');
+    })
+})
 
 /* Init F1 Connection configuration */
 const client = new F1TelemetryClient.F1TelemetryClient();
 
+
+/* Client info send functions */
+function sendPacket(type ,packet) {
+    io.emit(type, packet)
+}
+
 /* F1 Websocket connections */
 client.on('lapData', (data) => {
-    console.log(data)
+    if (DEBUG) console.log(data)
+    sendPacket('lapData', data)
 })
 
 client.on('carStatus', (data) => {
-    console.log(data)
+    if (DEBUG) console.log(data)
+    sendPacket('carStatus', data)
 })
 
 /* Start F1 Connection */
@@ -27,7 +45,7 @@ app.get('/', (req, res) => {
 
 /* Server Start */ 
 
-app.listen(port, () => {
-    console.log(`F1 Telemetry Client started listening on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`F1 Telemetry Client started listening on port ${PORT}`)
 })
 
