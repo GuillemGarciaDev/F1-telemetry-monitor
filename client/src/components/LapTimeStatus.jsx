1613e85479cc0e1f9ece0b515c18f4d609ed5ea0
Guillem García <guillem.garcia.gil@protonmail.com>
2022-06-25 12:07:47 +0200

    WIP Frontend: LapRaceStatus scheme added to main screen

---
+++ client/src/components/LapTimeStatus.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const LapTimeStatus = (props) => {
+
+    return (
+        <div class='flex flex-col p-2'>
+            <div class='flex flex-row items-center justify-center border-solid border-4 border-white rounded-md w-full h-20'>
+                <p class='font-f1Bold text-white text-lg'>
+                    50/70
+                </p>
+            </div>
+        </div>
+        
+    )
+}
+
+export default LapTimeStatus
