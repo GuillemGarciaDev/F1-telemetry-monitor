db853196d5c87c0577cfa9112958f7ec6c83b51e
Guillem García <68080871+GuillemGarciaDev@users.noreply.github.com>
2022-06-27 23:25:19 +0200

    Revert "LapRaceStatus component running 🔥"

--- server/constants/packets.js
+++
@@ -1,6 +0,0 @@
-exports.PACKETS = {
-    "carStatus": "carStatus",
-    "lapData": "lapData",
-    "participants": "participants",
-    "session": "session"
-}
--- server/utils/log.js
+++
@@ -1,5 +0,0 @@
-function getTime() {
-    return `${new Date().getDate()}-${new Date().getMonth()}-${new Date().getFullYear()} : ${ new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
-}
-
-exports.getTime = getTime
